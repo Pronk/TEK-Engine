f3ca9d05f3d06d9771a7828b7ee1eba762a662c4
Danila Fedorin <nothingnut@gmail.com>
2014-07-19 19:48:45 -0700

    Added LWJGL Function

--- src/com/thalamus/main/Thalamus.java
+++ src/com/thalamus/main/Thalamus.java
@@ -1,5 +1,34 @@
 package com.thalamus.main;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class Thalamus {
 
+	public Thalamus(){
+		initOpenGL();
+	}
+	
+	private void initOpenGL() {
+		try {
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.setTitle("Thalamus");
+			Display.create();
+			while(!Display.isCloseRequested()){
+				Display.update();
+				
+			}
+			Display.destroy();
+		} catch (LWJGLException e) {
+			Display.destroy();
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		new Thalamus();
+	}
+	
 }
